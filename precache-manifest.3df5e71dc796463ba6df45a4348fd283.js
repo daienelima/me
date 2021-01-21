87784ce650edcb155310de2e67a0c20ca0689a06
Daiene Lima <daiene.m17@gmail.com>
2021-01-20 21:17:08 -0300

    Updates

---
+++ precache-manifest.3df5e71dc796463ba6df45a4348fd283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c8c6b5a75ff820a90a8",
+    "url": "/me/static/css/main.fdf6a436.chunk.css"
+  },
+  {
+    "revision": "3c8c6b5a75ff820a90a8",
+    "url": "/me/static/js/main.3c8c6b5a.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "9ba680795c81ac1dff5d",
+    "url": "/me/static/js/2.9ba68079.chunk.js"
+  },
+  {
+    "revision": "30d5f4fe4c6dc3d525eeca033c9a51e6",
+    "url": "/me/index.html"
+  }
+];
